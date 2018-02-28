5233be87a2cb3518c3d4acd6f276dbcac0dcb3fb
colll78 <philipdisarro@gmail.com>
2018-02-28 15:04:22 -0500

    s
    s

--- Spawner.java
+++ Spawner.java
@@ -11,7 +11,7 @@ public class Spawner extends Actor
     int yRand = 0;
     public void EnemySpawn() {
 
-        if(getWorld().getObjects(GreenDude.class).size() < 150){
+        if(getWorld().getObjects(GreenDude.class).size() < 10){
             yRand = (Greenfoot.getRandomNumber(200) - 100);
             getWorld().addObject(new GreenDude(), getX(), getY()+ yRand);
         }
