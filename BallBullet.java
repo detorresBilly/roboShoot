cc63f118aa1f97805aca9f1c254fa877fdebd1f4
detorresBilly <detorresbilly@gmail.com>
2020-09-15 12:14:02 -0400

    fixed documentation

--- BallBullet.java
+++ BallBullet.java
@@ -3,8 +3,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * A ball bullet object, fired by enemies.
  * 
- * @author Philip DiSarro
- * @version 9/24/2019
+ * @author Billy DeTorres
+ * @version 9/15/2020
  */
 public class BallBullet extends EnemyBullet
 {
